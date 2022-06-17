5114a4fcd0cafb0712064181bc1e471305d7eb2c
GustavoLUF <gustavourio@gmail.com>
2022-06-17 01:55:04 -0300

    projeto Safety Place

---
+++ pagina.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function choseTemp(){
+    const temps = [26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
+    h1Temp=temps.sort()
+}
